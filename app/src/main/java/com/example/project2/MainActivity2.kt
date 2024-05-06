057535ed0843524d29167b49fbfc0b36117a66b1
luxolosobetwa <st10443381@vcconnect.edu.za>
2024-05-06 10:29:15 +0200

    final project

--- app/src/main/java/com/example/project2/MainActivity2.kt
+++ app/src/main/java/com/example/project2/MainActivity2.kt
@@ -1,16 +1,14 @@
 package com.example.project2
 
-import android.annotation.SuppressLint
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity2 : AppCompatActivity() {
 
-    private var feedCounter = 2
+    private var feedCounter = 0
     private var cleanCounter = 0
     private var happyCounter = 0
 
@@ -26,39 +24,31 @@ class MainActivity2 : AppCompatActivity() {
         val cleanEditText = findViewById<EditText>(R.id.cleanEditText)
         val happyEditText = findViewById<EditText>(R.id.happyEditText)
 
+        val imageView2: ImageView = findViewById(R.id.imageView2)
+
         feedButton.setOnClickListener {
             // Increment counter and update EditText
             feedCounter++
             feedEditText.setText(feedCounter.toString())
+
+            // Change ImageView to show image for "Feed"
+            imageView2.setImageResource(R.drawable.img_2)
         }
 
         cleanButton.setOnClickListener {
             // Increment counter and update EditText
             cleanCounter++
             cleanEditText.setText(cleanCounter.toString())
+
+            // Change ImageView to show image for "Clean"
+            imageView2.setImageResource(R.drawable.img_1)
         }
 
         happyButton.setOnClickListener {
             // Increment counter and update EditText
             happyCounter++
             happyEditText.setText(happyCounter.toString())
-        }
 
-        // Accessing views for image view
-        val imageView2: ImageView = findViewById(R.id.imageView2)
-
-        // Setting click listeners for buttons to change image view
-        feedButton.setOnClickListener {
-            // Change ImageView to show image for "Feed"
-            imageView2.setImageResource(R.drawable.img_2)
-        }
-
-        cleanButton.setOnClickListener {
-            // Change ImageView to show image for "Clean"
-            imageView2.setImageResource(R.drawable.img_1)
-        }
-
-        happyButton.setOnClickListener {
             // Change ImageView to show image for "Happy"
             imageView2.setImageResource(R.drawable.img)
         }
